57b155f22b2970883a968636ca03cf7c6ea4aaaa
Iresh Dissanayaka <socialexpz1@gmail.com>
2024-03-10 05:30:16 +0000

    implement order_resource

---
+++ examples/order/main.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    hashicups = {
+      source = "hashicorp.com/edu/hashicups"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "hashicups" {
+  username = "education"
+  password = "test123"
+  host     = "http://api:9090"
+}
+
+resource "hashicups_order" "edu" {
+  items = [
+    {
+      coffee = {
+        id = 3
+      }
+      quantity = 2
+    },
+    {
+      coffee = {
+        id = 1
+      }
+      quantity = 3
+    }
+  ]
+}
